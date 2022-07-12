86edbcc4d877dd6e4d9db1c3298397dba0cf18eb
Cristiano Alexandre de Faria <cristiano.alexandre@inatel.br>
2022-07-12 13:52:57 -0300

    42. Criando Banco de Dados automaticamente com SQLAlchemy

--- app.py
+++ app.py
@@ -4,7 +4,14 @@ from flask_restful import Api
 from resources.hotel import Hoteis, Hotel
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hotel.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
+
+
+@app.before_first_request
+def cria_banco():
+    banco.create_all()
 
 
 api.add_resource(Hoteis, '/hoteis')
@@ -12,4 +19,6 @@ api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
 
 
 if __name__ == '__main__':
+    from sql_alchemy import banco
+    banco.init_app(app)  # run only in main
     app.run(debug=True)
